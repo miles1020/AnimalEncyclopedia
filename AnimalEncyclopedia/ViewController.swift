aae014d85c13640a22e75758e104503ac5af1e5a
HelloM <a0968026393@gmail.com>
2020-05-06 14:49:11 +0800

    Initial Commit

---
+++ AnimalEncyclopedia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimalEncyclopedia
+//
+//  Created by HellöM on 2020/5/6.
+//  Copyright © 2020 HellöM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
